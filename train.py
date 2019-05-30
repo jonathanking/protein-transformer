3d5d65af3145d6de5c84263eef40db27d857a8b6
Jonathan King <jok120@pitt.edu>
2019-05-30 12:00:24 -0400

    Format command line args to use long/short forms.

--- train.py
+++ train.py
@@ -223,29 +223,30 @@ def main():
 
     # Training parameters
     parser.add_argument("-lr", "--learning_rate", type=float, default=1 * (10 ** -3))
-    parser.add_argument('-epochs', type=int, default=10)
+    parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument("-b", '--batch_size', type=int, default=8)
-    parser.add_argument('-early_stopping', type=int, default=None)
-    parser.add_argument('-n_warmup_steps', type=int, default=1000)
-    parser.add_argument('-clip', type=float, default=None)
-    parser.add_argument('-combined_loss', action='store_true',
+    parser.add_argument('--early_stopping', type=int, default=None,
+                        help="Stops if training hasn't improved in X epochs")
+    parser.add_argument('--n_warmup_steps', type=int, default=1000)
+    parser.add_argument('--clip', type=float, default=None)
+    parser.add_argument('--combined_loss', action='store_true',
                         help="Use a loss that combines (quasi-equally) DRMSD and MSE.")
 
     # Model parameters
-    parser.add_argument('-d_word_vec', type=int, default=20)
-    parser.add_argument('-d_model', type=int, default=256)
-    parser.add_argument('-d_inner_hid', type=int, default=1024)
-    parser.add_argument('-d_k', type=int, default=64)
-    parser.add_argument('-d_v', type=int, default=64)
-    parser.add_argument('-n_head', type=int, default=8)
-    parser.add_argument('-n_layers', type=int, default=6)
-    parser.add_argument('-dropout', type=float, default=0)
+    parser.add_argument('-dwv', '--d_word_vec', type=int, default=20)
+    parser.add_argument('-dm', '--d_model', type=int, default=512)
+    parser.add_argument('-dih', '--d_inner_hid', type=int, default=2048)
+    parser.add_argument('-dk', '--d_k', type=int, default=64)
+    parser.add_argument('-dv', '--d_v', type=int, default=64)
+    parser.add_argument('-nh', '--n_head', type=int, default=8)
+    parser.add_argument('-nl', '--n_layers', type=int, default=6)
+    parser.add_argument('-do', '--dropout', type=float, default=0)
 
     # Saving args
-    parser.add_argument('-log', default=None, nargs=1)
-    parser.add_argument('-save_mode', type=str, choices=['all', 'best'], default='best')
-    parser.add_argument('-print_loss', action='store_true')
-    parser.add_argument('-no_cuda', action='store_true')
+    parser.add_argument('--log', default=None, nargs=1)
+    parser.add_argument('--save_mode', type=str, choices=['all', 'best'], default='best')
+    parser.add_argument('--print_loss', action='store_true')
+    parser.add_argument('--no_cuda', action='store_true')
 
     opt = parser.parse_args()
     opt.cuda = not opt.no_cuda
