39e54e3aa3e5b606b99b9284359ae5d32a888888
Jonathan King <jok120@pitt.edu>
2019-08-06 10:03:21 -0400

    Add MissingBackboneAtoms and Sequence errors.

--- scripts/utils/structure_exceptions.py
+++ scripts/utils/structure_exceptions.py
@@ -10,3 +10,15 @@ class NonStandardAminoAcidError(Exception):
 
     def __init__(self, *args):
         super().__init__(*args)
+
+
+class MissingBackboneAtomsError(Exception):
+    """An exception to raise when a protein backbone is incomplete."""
+    def __init__(self, message):
+        self.message = message
+
+
+class SequenceError(Exception):
+    """An exception to raise when a sequence is not as expected."""
+    def __init__(self, *args):
+        super().__init__(*args)
