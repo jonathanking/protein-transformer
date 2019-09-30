d9dbf67d0b11d28266428753c5be24978a858026
Jonathan King <jok120@pitt.edu>
2019-09-30 13:46:27 -0400

    Clean up code. only require a list of metric to be hard coded.

--- scripts/plot.py
+++ scripts/plot.py
@@ -3,18 +3,12 @@ Plots a model's .train file.
 """
 import argparse
 from datetime import date
-
-import numpy
 import os
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 sns.set(style="darkgrid")
 from scipy.signal import savgol_filter
-
-def generate_pdbs(chkpt):
-    pass
-
 
 def get_model_name(filename):
     """
@@ -52,7 +46,7 @@ def plot(dftrain, dfval, metric, title, outpath, smoothing=False, skip=None, inc
     """ Simple plotting function. Given train/val data and options, saves fig outpath. """
     dftrain_new = dftrain.iloc[skip:]
 
-    sns.lineplot(x=dftrain_new.index, y=metric, data=dftrain_new, label=metric)
+    sns.lineplot(x=dftrain_new.index, y=metric, data=dftrain_new, label=metric, alpha=0.6)
     if smoothing:
         sns.lineplot(x=dftrain_new.index, y=smooth(dftrain_new[metric]), color="lightblue")
     if include_val:
@@ -65,10 +59,8 @@ def plot(dftrain, dfval, metric, title, outpath, smoothing=False, skip=None, inc
     plt.figure()
 
 
-
-
-
 def main():
+    # Setup
     df = get_df_from_file(args.train_file)
     dftrain = df[df["is_val"] != True]
     dfval = df[df["is_val"] & df["is_end_of_epoch"]]
@@ -78,20 +70,20 @@ def main():
     os.makedirs(outpath, exist_ok=True)
     smoothing = not args.no_smoothing
 
-    # Plots are defined first with (metric, plot_title)
-    normal_plots = [("drmsd", f"DRMSD Loss"),
-                    ("rmse", f"RMSE Loss"),
-                    ("combined", f"Combined Loss"),
-                    ("ln_drmsd", f"ln-DRMSD Loss")]
-    # Add (smoothing, include_val, skip), same for all plots
-    normal_plots = [p + (smoothing, args.val, None) for p in normal_plots]
+    # Plots are defined by a list of metrics
+    metrics = ["drmsd", "rmse", "combined", "ln_drmsd"]
+    titles = [m + " Loss" for m in metrics]
 
-    all_plots = normal_plots
+    # We then create 5-tuples to later feed into the plot fn. These contain:
+    #    -- (metric, plt_title, smoothing, include_val, skip)
+    all_plots = [(m, t, smoothing, args.val, None) for (m, t) in zip(metrics, titles)]
 
     # This plots are the same as normal_plots, but they start at iteration args.skip_first.
     if args.skip_first:
-        for (metric, plt_title, _, _, _) in normal_plots:
-            all_plots.append((metric, plt_title + f", t = [{args.skip_first}, )", smoothing, args.val, args.skip_first))
+        skip_plots = []
+        for (metric, plt_title, _, _, _) in all_plots:
+            skip_plots.append((metric, plt_title + f", t = [{args.skip_first}, )", smoothing, args.val, args.skip_first))
+        all_plots += skip_plots
 
     # Add model name to all plot titles
     all_plots = [(metric, model_name + "\n" + plt_title, s, v, skip) for (metric, plt_title, s, v, skip) in all_plots]
@@ -99,24 +91,6 @@ def main():
     # Make all plots
     for (metric, plt_title, sm, val, skip) in all_plots:
         plot(dftrain, dfval, metric, plt_title, outpath, smoothing=sm, include_val=val, skip=skip)
-
-    # # Normal plots
-    # plot(dftrain, dfval, "drmsd", f"{model_name}\nDRMSD Loss", outpath, smoothing=True, include_val=args.val)
-    # plot(dftrain, dfval, "rmse", f"{model_name}\nRMSE Loss", outpath, smoothing=True, include_val=args.val)
-    # plot(dftrain, dfval, "combined", f"{model_name}\nCombined Loss", outpath, smoothing=True, include_val=args.val)
-    # plot(dftrain, dfval, "ln_drmsd", f"{model_name}\nln-DRMSD Loss", outpath, smoothing=True, include_val=args.val)
-    #
-    # # Skip plots
-    # if args.skip_first:
-    #     plot(dftrain, dfval, "drmsd", f"{model_name}\nDRMSD Loss, t = [{args.skip_first}, )", outpath, smoothing=True,
-    #          include_val=args.val, skip=args.skip_first)
-    #     plot(dftrain, dfval, "rmse", f"{model_name}\nRMSE Loss, t = [{args.skip_first}, )", outpath, smoothing=True,
-    #          include_val=args.val, skip=args.skip_first)
-    #     plot(dftrain, dfval, "combined", f"{model_name}\nCombined Loss, t = [{args.skip_first}, )", outpath, smoothing=True,
-    #          include_val=args.val, skip=args.skip_first)
-    #     plot(dftrain, dfval, "ln_drmsd", f"{model_name}\nln-DRMSD Loss, t = [{args.skip_first}, )", outpath, smoothing=True,
-    #          include_val=args.val, skip=args.skip_first)
-
 
 
 if __name__ == "__main__":
