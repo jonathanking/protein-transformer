Fix bug with parallel drmsd. Must return gradients in child process. Refactor drmsd.
When the loss is computed in parallel over the batch, new processes are
made. Each process starts with a new graph. In this case, the input
is the angle tensor, and this tensor is now considered a leaf node.
We can take the angles, turn them into coordinates, compute drmsd,
and THEN when we call backward (still in the process), the input to the
parallelized loss function will have its .grad value populated. We can
then return this gradient value, along with the loss values (as scalars).
Then, we stack all of the gradients returned by the parallelized fns,
and use these to continue the backpropagation step from the angles
back through the rest of the model.

log.py
@@ -8,6 +8,7 @@ import torch
 from dataset import VOCAB
 from protein.Sidechains import NUM_PREDICTED_COORDS
 from protein.PDB_Creator import PDB_Creator
+from losses import angles_to_coords, inverse_trig_transform
 
 def print_train_batch_status(args, items):
     """
@@ -34,7 +35,7 @@ def print_train_batch_status(args, items):
             lr_string,
             speed=speed))
     else:
-        pbar.set_description('\r  - (Train) drmsd={drmsd:.2f}, lndrmsd={lnd:0.4f}, rmse={rmse:.4f},'
+        pbar.set_description('\r  - (Train) drmsd={drmsd:.2f}, lndrmsd={lnd:0.7f}, rmse={rmse:.4f},'
                              ' c={comb:.2f}{lr}, res/s={speed}'.format(
             drmsd=float(train_drmsd_loss),
             lr=lr_string,
@@ -132,7 +133,7 @@ def log_batch(log_writer, metrics, start_time,  mode="valid", end_of_epoch=False
 
 
 def do_train_batch_logging(metrics, d_loss, ln_d_loss, m_loss, c_loss, src_seq, loss, optimizer, args, log_writer, pbar,
-                           start_time, pred_coords, tgt_coords, step):
+                           start_time, pred_angs, tgt_coords, step):
     """
     Performs all necessary logging at the end of a batch in the training epoch.
     Updates custom metrics dictionary and wandb logs. Prints status of training.
@@ -159,7 +160,9 @@ def do_train_batch_logging(metrics, d_loss, ln_d_loss, m_loss, c_loss, src_seq, 
     if np.isnan(loss.item()):
         print("A nan loss has occurred. Exiting training.")
         sys.exit(1)
-    if do_log_str and pred_coords is not None:
+    if do_log_str:
+        with torch.no_grad():
+            pred_coords = angles_to_coords(inverse_trig_transform(pred_angs)[-1].cpu(), src_seq[-1].cpu(), remove_batch_padding=True)
         log_structure(args, pred_coords, tgt_coords, src_seq[-1])
 
 
@@ -169,7 +172,6 @@ def log_structure(args, pred_coords, gold_item, src_seq):
     """
     gold_item_non_batch_pad = (gold_item != VOCAB.pad_id).any(dim=-1)
     gold_item = gold_item[gold_item_non_batch_pad]
-
     creator = PDB_Creator(pred_coords.detach().numpy(), seq=VOCAB.indices2aa_seq(src_seq.cpu().detach().numpy()))
     creator.save_pdb(f"data/logs/structures/{args.name}_pred.pdb", title="pred")
     creator.save_gltf(f"data/logs/structures/{args.name}_pred.gltf")

losses.py
@@ -45,23 +45,22 @@ def remove_sos_eos_from_input(input_seq):
     return input_seq[start_idx : end_idx]
 
 
-def drmsd_work(ang_crd_seq):
+def drmsd_work(pred_ang, true_crd, input_seq, return_rmsd, do_backward=True):
     """
     A version of drmsd loss meant to be used in parallel. Operates on a tuple
     of predicted angles, coordinates, and sequence. Works for 1 protein at a
     time.
     """
-    pred_ang, true_crd, input_seq = ang_crd_seq
+    # Record leaf-node pointer to access gradients at end
+    starting_ang = pred_ang
+
     # Remove batch-level masking
     batch_mask = input_seq.ne(VOCAB.pad_id)
     input_seq = input_seq[batch_mask]
-    # Remove SOS and EOS characters if present
-    input_seq = remove_sos_eos_from_input(input_seq)
-    pred_ang = pred_ang[:input_seq.shape[0]]
     true_crd = true_crd[:input_seq.shape[0] * NUM_PREDICTED_COORDS]
 
-    # Generate coordinates
-    pred_crd = generate_coords(pred_ang, pred_ang.shape[0], input_seq, torch.device("cpu"))
+    # Compute coordinates
+    pred_crd = angles_to_coords(pred_ang, input_seq)
 
     # Remove coordinate-level masking for missing atoms
     true_crd_non_nan = torch.isnan(true_crd).eq(0)
@@ -71,108 +70,70 @@ def drmsd_work(ang_crd_seq):
     # Compute drmsd between existing atoms only
     loss = drmsd(pred_crds_masked, true_crds_masked)
     l_normed = loss / pred_crds_masked.shape[0]
-    return pred_crd, loss, l_normed
+
+    if do_backward:
+        l_normed.backward()
+
+    if return_rmsd:
+        return starting_ang.grad, loss.item(), l_normed.item(), rmsd(pred_crds_masked.data.numpy(),
+                                                                     true_crds_masked.data.numpy())
+    else:
+        return starting_ang.grad, loss.item(), l_normed.item()
 
 
-def drmsd_work(ang_crd_seq):
+def angles_to_coords(angles, seq, remove_batch_padding=False):
     """
-    A version of drmsd loss meant to be used in parallel. Operates on a tuple
-    of predicted angles, coordinates, and sequence. Works for 1 protein at a
-    time.
+    Convert torsional angles to coordinates.
     """
-    pred_ang, true_crd, input_seq = ang_crd_seq
-    # Remove batch-level masking
-    batch_mask = input_seq.ne(VOCAB.pad_id)
-    input_seq = input_seq[batch_mask]
+    pred_ang, input_seq = angles, seq
+    if remove_batch_padding:
+        # Remove batch-level masking
+        batch_mask = input_seq.ne(VOCAB.pad_id)
+        input_seq = input_seq[batch_mask]
+
     # Remove SOS and EOS characters if present
     input_seq = remove_sos_eos_from_input(input_seq)
     pred_ang = pred_ang[:input_seq.shape[0]]
-    true_crd = true_crd[:input_seq.shape[0] * NUM_PREDICTED_COORDS]
 
     # Generate coordinates
-    pred_crd = generate_coords(pred_ang, pred_ang.shape[0], input_seq, torch.device("cpu"))
-
-    # Remove coordinate-level masking for missing atoms
-    true_crd_non_nan = torch.isnan(true_crd).eq(0)
-    pred_crds_masked = pred_crd[true_crd_non_nan].reshape(-1, 3)
-    true_crds_masked = true_crd[true_crd_non_nan].reshape(-1, 3)
-
-    # Compute drmsd between existing atoms only
-    loss = drmsd(pred_crds_masked, true_crds_masked)
-    l_normed = loss / pred_crds_masked.shape[0]
-    return pred_crd, loss, l_normed
+    return generate_coords(pred_ang, pred_ang.shape[0], input_seq, torch.device("cpu"))
 
 
-def drmsd_rmsd_work(ang_crd_seq):
-    """
-    Differs from drmsd_work only in that it also computes/returns rmsd.
-    A version of drmsd loss meant to be used in parallel. Operates on a tuple
-    of predicted angles, coordinates, and sequence. Works for 1 protein at a
-    time.
-    """
-    pred_ang, true_crd, input_seq = ang_crd_seq
-    # Remove batch-level masking
-    batch_mask = input_seq.ne(VOCAB.pad_id)
-    input_seq = input_seq[batch_mask]
-    # Remove SOS and EOS characters if present
-    input_seq = remove_sos_eos_from_input(input_seq)
-    pred_ang = pred_ang[:input_seq.shape[0]]
-    true_crd = true_crd[:input_seq.shape[0] * NUM_PREDICTED_COORDS]
-
-    # Generate coordinates
-    pred_crd = generate_coords(pred_ang, pred_ang.shape[0], input_seq, torch.device("cpu"))
-
-    # Remove coordinate-level masking for missing atoms
-    true_crd_non_nan = torch.isnan(true_crd).eq(0)
-    pred_crds_masked = pred_crd[true_crd_non_nan].reshape(-1, 3)
-    true_crds_masked = true_crd[true_crd_non_nan].reshape(-1, 3)
-
-    # Compute drmsd between existing atoms only
-    loss = drmsd(pred_crds_masked, true_crds_masked)
-    l_normed = loss / pred_crds_masked.shape[0]
-    return pred_crd, loss, l_normed, rmsd(pred_crds_masked.data.numpy(), true_crds_masked.data.numpy())
-
-
-
-
-def drmsd_loss_from_angles(pred_angs, true_crds, input_seqs, device, return_rmsd=False):
+def compute_batch_drmsd(pred_angs, true_crds, input_seqs, device=torch.device("cpu"), return_rmsd=False,
+                        do_backward=False, retain_graph=False):
     """
     Calculate DRMSD loss by first generating predicted coordinates from
     angles. Then, predicted coordinates are compared with the true coordinate
     tensor provided to the function.
     """
-
     pred_angs, true_crds, input_seqs = pred_angs.to(device), true_crds.to(device), input_seqs.to(device)
 
     pred_angs = inverse_trig_transform(pred_angs)
 
-    losses = []
-    len_normalized_losses = []
-    rmsds = []
-    if not return_rmsd:
-        fn = drmsd_work
-    else:
-        fn = drmsd_rmsd_work
-
     # Compute drmsd in parallel over the batch
     results = Parallel(n_jobs=multiprocessing.cpu_count(),
-                       backend="loky")(delayed(fn)(a_c_s)
-                                       for a_c_s in zip(pred_angs, true_crds, input_seqs))
+                       backend="loky")(delayed(drmsd_work)(ang, crd, seq, return_rmsd, do_backward)
+                                       for ang, crd, seq in zip(pred_angs, true_crds, input_seqs))
 
     # Unpack the multiprocessing results
+    grads, losses, len_normalized_losses, rmsds = [], [], [], []
     for r in results:
         if len(r) == 4:
-            pred_crd, l, ln, rmsd_val = r
+            grad, l, ln, rmsd_val = r
             rmsds.append(rmsd_val)
         else:
-            pred_crd, l, ln = r
+            grad, l, ln = r
+        grads.append(grad)
         losses.append(l)
         len_normalized_losses.append(ln)
 
+    if do_backward:
+        pred_angs.backward(gradient=torch.stack(grads), retain_graph=retain_graph)
+
     if return_rmsd:
-        return pred_crd, torch.mean(torch.stack(losses)), torch.mean(torch.stack(len_normalized_losses)), np.mean(rmsds)
+        return np.mean(losses), np.mean(len_normalized_losses), np.mean(rmsds)
     else:
-        return pred_crd, torch.mean(torch.stack(losses)), torch.mean(torch.stack(len_normalized_losses))
+        return np.mean(losses), np.mean(len_normalized_losses)
 
 
 def mse_over_angles(pred, true):

train.py
@@ -17,12 +17,14 @@ from tqdm import tqdm
 import wandb
 
 from dataset import prepare_dataloaders
-from losses import drmsd_loss_from_angles, mse_over_angles, combine_drmsd_mse
+from losses import compute_batch_drmsd, mse_over_angles, combine_drmsd_mse
 from models.transformer.Transformer import Transformer
 from models.transformer.Optimizer import ScheduledOptim
 from log import *
 from models.encoder_only import EncoderOnlyTransformer
 from protein.Sidechains import NUM_PREDICTED_ANGLES
+
+
 
 
 def train_epoch(model, training_data, optimizer, device, args, log_writer, metrics):
@@ -40,8 +42,7 @@ def train_epoch(model, training_data, optimizer, device, args, log_writer, metri
         if args.skip_missing_res_train and torch.isnan(tgt_ang).all(dim=-1).any().byte():
             continue
         pred = model(src_seq, tgt_ang)
-        loss, d_loss, ln_d_loss, m_loss, c_loss, pred_coords = get_losses(args, step, device, pred, tgt_ang, tgt_crds, src_seq)
-        loss.backward()
+        loss, d_loss, ln_d_loss, m_loss, c_loss = get_losses(args, pred, tgt_ang, tgt_crds, src_seq)
 
         # Clip gradients
         if args.clip:
@@ -52,7 +53,7 @@ def train_epoch(model, training_data, optimizer, device, args, log_writer, metri
 
         # Record performance metrics
         do_train_batch_logging(metrics, d_loss, ln_d_loss, m_loss, c_loss, src_seq, loss, optimizer, args,
-                               log_writer, batch_iter, START_TIME, pred_coords, tgt_crds[-1], step)
+                               log_writer, batch_iter, START_TIME, pred, tgt_crds[-1], step)
         n_batches += 1
 
     metrics = update_metrics_end_of_epoch(metrics, "train", n_batches)
@@ -60,36 +61,39 @@ def train_epoch(model, training_data, optimizer, device, args, log_writer, metri
     return metrics
 
 
-def get_losses(args, step, device, pred, tgt_ang, tgt_crds, src_seq):
+def get_losses(args, pred, tgt_ang, tgt_crds, src_seq):
     """
     Returns the computed losses/metrics for a batch. The variable 'loss'
     will differ depending on the loss the user requested to train on.
     """
+    # TODO remove outdated reference to loss
+    # Always compute MSE loss b/c it's computationally cheap.
     m_loss = mse_over_angles(pred, tgt_ang)
 
     if args.loss == "ln-drmsd":
-        pred_coords, d_loss, ln_d_loss = drmsd_loss_from_angles(pred, tgt_crds, src_seq, torch.device('cpu'))
-        c_loss = combine_drmsd_mse(ln_d_loss.to(device), m_loss, w=args.combined_drmsd_weight)
+        d_loss, ln_d_loss = compute_batch_drmsd(pred, tgt_crds, src_seq, do_backward=True, retain_graph=False)
+        c_loss = combine_drmsd_mse(ln_d_loss, m_loss, w=args.combined_drmsd_weight)
         loss = ln_d_loss
-    elif args.loss == "mse" and step % args.log_structure_step != 0:
-        # Other losses are not computed for computational efficiency.
-        pred_coords, d_loss, ln_d_loss = None, torch.tensor(0), torch.tensor(0)
-        c_loss = torch.tensor(0)
-        loss = m_loss
-    elif args.loss == "mse" and step % args.log_structure_step == 0:
-        pred_coords, d_loss, ln_d_loss = drmsd_loss_from_angles(pred, tgt_crds, src_seq, torch.device('cpu'))
-        c_loss = combine_drmsd_mse(ln_d_loss.to(device), m_loss, w=args.combined_drmsd_weight)
-        loss = m_loss
-    elif args.loss == "drmsd":
-        pred_coords, d_loss, ln_d_loss = drmsd_loss_from_angles(pred, tgt_crds, src_seq, torch.device('cpu'))
-        c_loss = combine_drmsd_mse(ln_d_loss.to(device), m_loss, w=args.combined_drmsd_weight)
-        loss = d_loss
-    else:
-        pred_coords, d_loss, ln_d_loss = drmsd_loss_from_angles(pred, tgt_crds, src_seq, torch.device('cpu'))
-        c_loss = combine_drmsd_mse(ln_d_loss.to(device), m_loss, w=args.combined_drmsd_weight)
-        loss = c_loss
 
-    return loss, d_loss, ln_d_loss, m_loss, c_loss, pred_coords
+    elif args.loss == "mse":
+        # Other losses are not computed for computational efficiency.
+        d_loss, ln_d_loss, c_loss = torch.tensor(0), torch.tensor(0), torch.tensor(0)
+        loss = m_loss
+        m_loss.backward()
+
+    elif args.loss == "drmsd":
+        d_loss, ln_d_loss = compute_batch_drmsd(pred, tgt_crds, src_seq, do_backward=True, retain_graph=False)
+        c_loss = combine_drmsd_mse(ln_d_loss, m_loss, w=args.combined_drmsd_weight)
+        loss = d_loss
+
+    else:
+        # Combined loss
+        d_loss, ln_d_loss = compute_batch_drmsd(pred, tgt_crds, src_seq, do_backward=True, retain_graph=True)
+        c_loss = combine_drmsd_mse(ln_d_loss, m_loss, w=args.combined_drmsd_weight)
+        loss = c_loss
+        c_loss.backward()
+
+    return loss, d_loss, ln_d_loss, m_loss, c_loss
 
 
 def eval_epoch(model, validation_data, device, args, metrics, mode="valid"):
@@ -105,9 +109,9 @@ def eval_epoch(model, validation_data, device, args, metrics, mode="valid"):
         for batch in batch_iter:
             src_seq, tgt_ang, tgt_crds = map(lambda x: x.to(device), batch)
             pred = model(src_seq, tgt_ang)
-            pred_coords, d_loss, ln_d_loss, r_loss = drmsd_loss_from_angles(pred, tgt_crds, src_seq, torch.device("cpu"), return_rmsd=True)
+            d_loss, ln_d_loss, r_loss = compute_batch_drmsd(pred, tgt_crds, src_seq, return_rmsd=True)
             m_loss = mse_over_angles(pred, tgt_ang)
-            c_loss = combine_drmsd_mse(ln_d_loss.to(device), m_loss, w=args.combined_drmsd_weight)
+            c_loss = combine_drmsd_mse(ln_d_loss, m_loss, w=args.combined_drmsd_weight)
             do_eval_epoch_logging(metrics, d_loss, ln_d_loss, m_loss, c_loss, r_loss, src_seq, args, batch_iter, mode)
             n_batches += 1
 
@@ -183,7 +187,8 @@ def checkpoint_model(args, optimizer, model, metrics, epoch_i):
 
     torch.save(checkpoint, chkpt_file_name)
     wandb.save(chkpt_file_name)
-    wandb.run.summary["avg_evaluation_speed"] = np.mean(metrics["valid"]["speed-history"])
+    if not args.train_only:
+        wandb.run.summary["avg_evaluation_speed"] = np.mean(metrics["valid"]["speed-history"])
     wandb.run.summary["avg_training_speed"] = np.mean(metrics["train"]["speed-history"])
     metrics["last_chkpt_time"] = time.time()
     print('\r    - [Info] The checkpoint file has been updated.')
@@ -276,6 +281,8 @@ def seed_rngs(args):
     torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
+    if torch.backends.cudnn.deterministic:
+        print("[Info] cudnn.deterministic set to True. CUDNN-optimized code may be slow.")
 
 
 def main():