afe8defa090121947130ef0bc861b847503de43c
jonathanking <jonathanking@berkeley.edu>
2019-11-09 14:29:02 -0500

    Create pytest files.

---
+++ protein_transformer/tests/test_protein_transformer.py
@@ -0,0 +1,12 @@
+"""
+Unit and regression test for the protein_transformer package.
+"""
+
+# Import package, test suite, and other packages as needed
+import protein_transformer
+import pytest
+import sys
+
+def test_protein_transformer_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "protein_transformer" in sys.modules
