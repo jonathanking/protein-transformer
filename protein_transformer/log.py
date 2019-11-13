a1fa970ae96aa9cb73d5e7d32333dad0b485abb3
jonathanking <jonathanking@berkeley.edu>
2019-11-13 09:34:28 -0500

    Remove redunant reference to do_eval_epoch_logging.

--- protein_transformer/log.py
+++ protein_transformer/log.py
@@ -247,21 +247,6 @@ def log_structure(args, pred_coords, gold_item, src_seq):
     wandb.log({"structure_prediction": wandb.Object3D(f"../data/logs/structures/{args.name}_pred.gltf")}, commit=True)
 
 
-def do_eval_epoch_logging(metrics, d_loss, ln_d_loss, m_loss, c_loss, r_loss, src_seq, args, pbar, mode):
-    """
-    Performs all necessary logging at the end of an evaluation batch.
-    Updates custom metrics dictionary and wandb logs. Prints status of training.
-    """
-    metrics = update_metrics(metrics, mode, d_loss, ln_d_loss, m_loss, c_loss, src_seq, r_loss, batch_level=False)
-    wandb.log({f"{mode.title()} Epoch RMSE": np.sqrt(m_loss.item()),
-               f"{mode.title()} Epoch RMSD": r_loss,
-               f"{mode.title()} Epoch DRMSD": d_loss,
-               f"{mode.title()} Epoch ln-DRMSD": ln_d_loss,
-               f"{mode.title()} Epoch Combined Loss": c_loss,
-               f"{mode.title()} Epoch Speed": metrics[mode]["speed"]})
-    print_eval_batch_status(args, (pbar, d_loss, mode, m_loss, c_loss))
-
-
 def init_metrics(args):
     """
     Returns an empty metric dictionary for recording model performance.
