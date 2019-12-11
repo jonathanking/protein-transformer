19f339f9ec88d4e1a3cd10e51496f7132fd3c83d
Jonathan King <jok120@pitt.edu>
2019-12-11 17:37:47 -0500

    Create slurm submission script.

--- research/cluster/191211_pbs2slurm.sh
+++ research/cluster/191211_pbs2slurm.sh
@@ -10,39 +10,37 @@
 
 
 ############################
+##       Conda Setup      ##
+############################
+#__conda_setup="$('/net/mahler/opt/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+#if [ $? -eq 0 ]; then
+#   eval "$__conda_setup"
+#else
+#    if [ -f "/net/mahler/opt/anaconda3/etc/profile.d/conda.sh" ]; then
+#        . "/net/mahler/opt/anaconda3/etc/profile.d/conda.sh"
+#    else
+#        export PATH="/net/mahler/opt/anaconda3/bin:$PATH"
+#    fi
+#fi
+#unset __conda_setup
+eval "$(cat ~/bin/conda_init.sh)"
+
+
+############################
 ##       Environment      ##
 ############################
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/net/mahler/opt/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-   eval "$__conda_setup"
-else
-    if [ -f "/net/mahler/opt/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/net/mahler/opt/anaconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/net/mahler/opt/anaconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
-
-
-cd $SLURM_SUBMIT_DIR
 export PATH=/usr/local/bin:$PATH
 export LD_LIBRARY_PATH=LD_LIBRARY_PATH:/usr/local/cuda-9.0/lib64/
 conda activate pytorch-build
 which python
 
+
 ############################
 ##     Array Job Exec.    ##
 ############################
+cd $SLURM_SUBMIT_DIR
 cmd="$(sed -n "${SLURM_ARRAY_TASK_ID}p" research/cluster/191211_test.txt)"
 echo $cmd
 eval $cmd
 
-
-############################
-##          Exit          ##
-############################
 exit 0
