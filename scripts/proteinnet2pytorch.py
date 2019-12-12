3aaba7e71d77eb62d0bfdd2dda6f3e91477337b5
jonathanking <jonathanking@berkeley.edu>
2019-12-12 09:31:21 -0500

    WIP. Incorporate Hunter's modifications. Untested.

--- scripts/proteinnet2pytorch.py
+++ scripts/proteinnet2pytorch.py
@@ -1,8 +1,7 @@
 """
-Using ProteinNet as a guide, this script creates a new dataset that adds
-sidechain atom information. It retains data splits, sequences, and masks but
-recomputes each structure's coordinates so that sidechain atoms may be
-recorded. It also saves the entirety of the data as a single Python dictionary.
+    Using ProteinNet as a guide, this script creates a new dataset that adds sidechain atom information.
+    It retains data splits, sequences, and masks but recomputes each structure's coordinates so that
+    sidechain atoms may be recorded. It also saves the entirety of the data as a single Python dictionary.
 
     Author: Jonathan King
     Date:   July 21, 2019
@@ -21,14 +20,12 @@ import prody as pr
 import torch
 import tqdm
 
-sys.path.append("/home/jok120/protein-transformer/scripts/utils/")
-sys.path.append("../scripts/utils")
-sys.path.extend("../protein/")
-from protein.structure_utils import angle_list_to_sin_cos, seq_to_onehot, get_seq_and_masked_coords_and_angles, \
+sys.path.append(".")
+from protein_transformer.protein.structure_utils import angle_list_to_sin_cos, seq_to_onehot, get_seq_and_masked_coords_and_angles, \
     no_nans_infs_allzeros, parse_astral_summary_file, \
     get_chain_from_astral_id, get_header_seq_from_astral_id, GLOBAL_PAD_CHAR
-from scripts.utils.proteinnet_parsing import parse_raw_proteinnet
-from scripts.utils.structure_exceptions import IncompleteStructureError, NonStandardAminoAcidError, SequenceError, ContigMultipleMatchingError, ShortStructureError
+import proteinnet_parsing
+from protein_transformer.protein.structure_exceptions import IncompleteStructureError, NonStandardAminoAcidError, SequenceError, ContigMultipleMatchingError, ShortStructureError, MissingAtomsError
 
 
 
@@ -41,12 +38,18 @@ PARSING_ERRORS = m.list()
 NSAA_ERRORS = m.list()
 MISSING_ASTRAL_IDS = m.list()
 SHORT_ERRORS = m.list()
-
+PARSING_ERROR_ATTRIBUTE = m.list()
+PARSING_ERROR = m.list()
+PARSING_ERROR_OSERROR = m.list()
+UNKNOWN_EXCEPTIONS = m.list()
+MISSING_ATOMS_ERROR = m.list()
+NONE_CHAINS = m.list()
+NO_PBD_FILE = m.list()
 
 def get_chain_from_trainid(proteinnet_id):
     """
     Given a ProteinNet ID of a training or validation set item, this function returns the associated
-    ProDy-parsed chain object. "1A9U_2_A" ("4EDJ_djsldfj-")
+    ProDy-parsed chain object. "1A9U_2_A"
     """
     # Try parsing the ID as a PDB ID. If it fails, assume it's an ASTRAL ID.
     try:
@@ -58,30 +61,41 @@ def get_chain_from_trainid(proteinnet_id):
             pdbid, astral_id = proteinnet_id.split("_")
             return get_chain_from_astral_id(astral_id.replace("-", "_"), ASTRAL_ID_MAPPING)
         except KeyError:
-            MISSING_ASTRAL_IDS.append(1)
+            MISSING_ASTRAL_IDS.append(proteinnet_id)
             return None
         except ValueError:
-            FAILED_ASTRAL_IDS.append(1)
+            FAILED_ASTRAL_IDS.append(proteinnet_id)
             return None
         except:
-            FAILED_ASTRAL_IDS.append(1)
+            FAILED_ASTRAL_IDS.append(proteinnet_id)
             return None
 
     # Continue loading the chain, given the PDB ID
     try:
-        pdb_hv = pr.parsePDB(pdbid, chain=chid).getHierView()
-    except (AttributeError, pr.proteins.pdbfile.PDBParseError, OSError) as e:
-        # TODO split up errors to determine each one?
-        PARSING_ERRORS.append(1)
+        chain = pr.parseCIF(pdbid, chain=chid) # changed pr.parsePDB to pr.parseCIF, removed heirarchal view
+    except AttributeError:
+        PARSING_ERROR_ATTRIBUTE.append(proteinnet_id)
         return None
-    chain = pdb_hv[chid]
+    except pr.proteins.pdbfile.PDBParseError:
+        PARSING_ERROR.append(proteinnet_id)
+        return None
+    except OSError:
+        PARSING_ERROR_OSERROR.append(proteinnet_id)
+        return None
+    except Exception as e:
+        UNKNOWN_EXCEPTIONS.append(str(e) + " " + proteinnet_id)
+        return None
 
+    if chain is None:
+        print(proteinnet_id)
+        NONE_CHAINS.append(proteinnet_id)
+        return None
     # Attempt to select a coordset
     try:
         if chain.numCoordsets() > 1:
             chain.setACSIndex(int(model_id))
     except IndexError:
-        # TODO is this appropriate to pass?
+        # TODO is this appropriate to pass? make global variable that returns same kinda error as above. happens a lot
         pass
 
     return chain
@@ -95,14 +109,15 @@ def get_chain_from_testid(proteinnet_id):
     # TODO: assert existence of test/targets at start of script
     category, caspid = proteinnet_id.split("#")
     try:
-        pdb_hv = pr.parsePDB(os.path.join(args.input_dir, "targets", caspid + ".pdb")).getHierView()
+        pdb_hv = pr.parsePDB(os.path.join(args.input_dir, "targets", caspid + ".pdb")).getHierView() # TODO change to pr.parseCIF?
     except AttributeError:
-        PARSING_ERRORS.append(1)
+        PARSING_ERRORS.append(proteinnet_id)
         return None
     try:
         assert pdb_hv.numChains() == 1
     except:
         print("Only a single chain should be parsed from the CASP targ PDB.")
+        pass
     chain = next(iter(pdb_hv))
     return chain
 
@@ -133,65 +148,39 @@ def get_proteinnet_seq_from_id(pnid):
     return true_seq
 
 
-def get_sequence_from_pdb_header(pnid):
-    """ Given a ProteinNet ID, this function tries to obtain the sequence from the PDB's
-        SEQRES records."""
-    # Try parsing the file, assuming it's not an ASTRAL ID.
-    try:
-        pdbid, model_id, chid = pnid.split("_")
-        if "#" in pdbid:
-            pdbid = pdbid.split("#")[1]
-        p, header = pr.parsePDB(pdbid, chain=chid, header=True)
-        polymer = header[chid]
-        return polymer.sequence
-    except ValueError:
-        # This means the pnid actually refers to an ASTRAL id
-        pdbid, astral_id = pnid.split("_")
-        return get_header_seq_from_astral_id(astral_id, ASTRAL_ID_MAPPING)
-
-
 def work(pdbid_chain):
     """
-    For a single PDB ID with chain, i.e. ('1A9U_A'), fetches that PDB chain from the PDB and
+    For a single PDB ID with chain, i.e. ('1A9U_1_A'), fetches that PDB chain from the PDB and
     computes its angles, coordinates, and sequence. The angles and coordinates contain
     GLOBAL_PAD_CHARs where there was missing data.
     """
-    true_seq = get_proteinnet_seq_from_id(pdbid_chain)
+    true_seq = get_proteinnet_seq_from_id(pdbid_chain) # TODO replace this function that returns chain from file w/ seq
     chain = get_chain_from_proteinnetid(pdbid_chain)  # Returns ProDy chain object
     if chain is None:
         return None
     try:
         dihedrals_coords_sequence = get_seq_and_masked_coords_and_angles(chain, true_seq)
     except NonStandardAminoAcidError:
-        NSAA_ERRORS.append(1)  # TODO Change errors to record chain name instead of '1'
-        # NSAA_ERRORS.append(pdbid_chain)
+        NSAA_ERRORS.append(pdbid_chain)
         return None
     except ContigMultipleMatchingError:
-        MULTIPLE_CONTIG_ERRORS.append(1)
+        MULTIPLE_CONTIG_ERRORS.append(pdbid_chain)
         return None
     except ShortStructureError:
-        SHORT_ERRORS.append(1)
+        SHORT_ERRORS.append(pdbid_chain)
+        return None
+    except MissingAtomsError:
+        MISSING_ATOMS_ERROR.append(pdbid_chain)
         return None
     except SequenceError:
-        # Only fails in complex masking cases.
-        # This means there was some mismatch between the "true" sequence and observed sequence.
-        # Since the default true seq comes from ProteinNet, this attempts to use the SEQRES records.
-        try:
-            seq = get_sequence_from_pdb_header(pdbid_chain)
-            dihedrals_coords_sequence = get_seq_and_masked_coords_and_angles(chain, seq)
-        except SequenceError:
-            print("Not fixed.", pdbid_chain)
-            SEQUENCE_ERRORS.append(1)
-            return None
-        except ContigMultipleMatchingError:
-            MULTIPLE_CONTIG_ERRORS.append(1)
-            return None
+        print("Not fixed.", pdbid_chain)
+        SEQUENCE_ERRORS.append(pdbid_chain)
+        return None
 
     # If we've made it this far, we can unpack the data and return it
     dihedrals, coords, sequence = dihedrals_coords_sequence
 
     return dihedrals, coords, sequence, pdbid_chain
-
 
 def unpack_processed_results(results):
     """
@@ -209,8 +198,9 @@ def unpack_processed_results(results):
             # PDB failed to download
             continue
         ang, coords, seq, i = r
-        if no_nans_infs_allzeros(ang) and no_nans_infs_allzeros(coords):
-            all_ohs.append(seq)
+        oh = seq_to_onehot(seq)
+        if no_nans_infs_allzeros(oh) and no_nans_infs_allzeros(ang) and no_nans_infs_allzeros(coords):
+            all_ohs.append(oh)
             all_angs.append(ang)
             all_crds.append(coords)
             all_ids.append(i)
@@ -226,12 +216,12 @@ def validate_data_dict(data):
     # Assert size of each data subset matches
     train_len = len(data["train"]["seq"])
     test_len = len(data["test"]["seq"])
-    assert all([l == train_len
-                for l in map(len, [data["train"][k]
-                                   for k in ["ang", "ids"]])]), "Train lengths don't match."
-    assert all([l == test_len
-                for l in map(len, [data["test"][k]
-                                   for k in ["ang", "ids"]])]), "Test lengths don't match."
+    items_recorded = ["seq", "ang", "ids", "crd"]
+    for num_items, subset in zip([train_len, test_len], ["train", "test"]):
+        assert all([l == num_items
+                    for l in map(len, [data[subset][k]
+                                       for k in items_recorded])]), f"{subset} lengths don't match."
+
     for split in VALID_SPLITS:
         valid_len = len(data["valid"][split]["seq"])
         assert all([l == valid_len for l in map(len, [data["valid"][split][k] for k in ["ang", "ids", "crd"]])]), \
@@ -258,7 +248,7 @@ def create_data_dict(train_seq, test_seq, train_ang, test_ang, train_crd, test_c
                          "pad_char": GLOBAL_PAD_CHAR},
             "description": {f"ProteinNet {CASP_VERSION.upper()}"},
             # To parse date later, use datetime.datetime.strptime(date, "%I:%M%p on %B %d, %Y")
-            "date": datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")}
+            "date": {datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")}}
     max_val_len = 0
     for split, (seq_val, ang_val, crd_val, ids_val) in all_validation_data.items():
         data["valid"][split]["seq"] = seq_val
@@ -294,14 +284,14 @@ def save_data_dict(data):
     Saves a Python dictionary containing all training data to disk via Pickle or PyTorch.
     """
     if not args.out_file:
-        args.out_file = "/home/jok120/protein-transformer/data/proteinnet/" + CASP_VERSION + "_" + SUFFIX + ".pt"
+        args.out_file = "/home/hth6/Downloads/protein-transformer/data/proteinnet/" + CASP_VERSION + "_" + SUFFIX + ".pt"
     torch.save(data, args.out_file)
     print(f"Data saved to {args.out_file}.")
 
 
 def main():
     global PN_TRAIN_DICT, PN_VALID_DICT, PN_TEST_DICT
-    train_pdb_ids, valid_ids, test_casp_ids = parse_raw_proteinnet(args.input_dir, TRAIN_FILE)
+    train_pdb_ids, valid_ids, test_casp_ids = proteinnet_parsing.parse_raw_proteinnet(args.input_dir, TRAIN_FILE)
     print("IDs fetched.")
     PN_TRAIN_DICT, PN_VALID_DICT, PN_TEST_DICT = torch.load(
         os.path.join(args.input_dir, "torch", TRAIN_FILE)), torch.load(
@@ -310,17 +300,19 @@ def main():
 
     # Download and preprocess all data from PDB IDs
     lim = None
-    with Pool(multiprocessing.cpu_count()) as p:
+    train_results = []
+    with multiprocessing.Pool(multiprocessing.cpu_count()) as p:
         train_results = list(tqdm.tqdm(p.imap(work, train_pdb_ids[:lim]), total=len(train_pdb_ids[:lim])))
-
     valid_result_meta = {}
     for split, vids in group_validation_set(valid_ids).items():
-        with Pool(multiprocessing.cpu_count()) as p:
-            valid_results = list(tqdm.tqdm(p.imap(work, vids), total=len(vids)))
-        valid_result_meta[split] = valid_results
+        valid_result_meta[split] = []
+        for vid in tqdm.tqdm(vids):
+            valid_result_meta[split].append(work(vid))
 
-    with Pool(multiprocessing.cpu_count()) as p:
-        test_results = list(tqdm.tqdm(p.imap(work, test_casp_ids), total=len(test_casp_ids)))
+    test_results = []
+    for tid in tqdm.tqdm(test_casp_ids):
+        test_results.append(work(tid))
+
     print("Structures processed.")
     print_failure_summary()
 
@@ -348,8 +340,52 @@ def print_failure_summary():
     print(f"{len(NSAA_ERRORS)} ProteinNet IDs failed because of non-std AAs.")
     print(f"{len(SHORT_ERRORS)} ProteinNet IDs failed because their length was <= 2.")
     print(f"{len(PARSING_ERRORS)} ProteinNet IDs failed because of parsing errors.")
-    # TODO inspect actual erroneous IDs
+    print(f"{len(PARSING_ERROR_ATTRIBUTE)} ProteinNet IDs failed because of attribute errors.")
+    print(f"{len(PARSING_ERROR)} ProteinNet IDs failed because of standard parsing error.")
+    print(f"{len(PARSING_ERROR_OSERROR)} ProteinNet IDs failed because of OSError.")
+    print(f"{len(UNKNOWN_EXCEPTIONS)} ProteinNet IDs failed because of unknown OSError.")
+    print(f"{len(MISSING_ATOMS_ERROR)} ProteinNet IDs failed because residues were missing N, CA1, or C atoms.")
+    print(f"{len(NONE_CHAINS)} ProteinNet IDs failed because no chain existed.")
+    print(f"{len(NO_PBD_FILE)} ProteinNet IDs failed because no PDB file exists and they probably have .CIF instead.")
 
+
+    with open('newErrors/MISSING_ASTRAL_IDS.txt', 'w') as f:
+        f.write('\n'.join(MISSING_ASTRAL_IDS))
+    with open('newErrors/FAILED_ASTRAL_IDS.txt', 'w') as f:
+        f.write('\n'.join(FAILED_ASTRAL_IDS))
+    with open('newErrors/MULTIPLE_CONTIG_ERRORS.txt', 'w') as f:
+        f.write('\n'.join(MULTIPLE_CONTIG_ERRORS))
+    with open('newErrors/SEQUENCE_ERRORS.txt', 'w') as f:
+        f.write('\n'.join(SEQUENCE_ERRORS))
+    with open('newErrors/NSAA_ERRORS.txt', 'w') as f:
+        f.write('\n'.join(NSAA_ERRORS))
+    with open('newErrors/SHORT_ERRORS.txt', 'w') as f:
+        f.write('\n'.join(SHORT_ERRORS))
+    #Total number of parsing errors, may not be needed
+    with open('newErrors/PARSING_ERRORS.txt', 'w') as f:
+        f.write('\n'.join(PARSING_ERRORS))
+    #Added to split up the three category of parsing errors, this one handles attribute errors
+    with open('newErrors/PARSING_ERROR_ATTRIBUTE.txt', 'w') as f:
+        f.write('\n'.join(PARSING_ERROR_ATTRIBUTE))
+    #Added to handle 'regular' parsing errors
+    with open('newErrors/PARSING_ERROR.txt', 'w') as f:
+        f.write('\n'.join(PARSING_ERROR))
+    #Added to handle OS related parsing errors
+    with open('newErrors/PARSING_ERROR_OSERROR.txt', 'w') as f:
+        f.write('\n'.join(PARSING_ERROR_OSERROR))
+
+    #Added for cases where pr.parseCIF returned unknown exception in function get_chain_from_trainid(proteinnet_id)
+    with open('newErrors/UNKNOWN_EXCEPTION.txt', 'w') as f:
+        f.write('\n'.join(UNKNOWN_EXCEPTIONS))
+    #Added for cases where measure_phi_psi_omega or measure_bond_angles fail due to missing atom information
+    with open('newErrors/MISSING_ATOMS_ERRORS.txt', 'w') as f:
+        f.write('\n'.join(MISSING_ATOMS_ERROR))
+    #Added when chain variable returned 'none' in function get_chain_from_trainid(proteinnet_id)
+    with open('newErrors/NONE_CHAINS.txt', 'w') as f:
+        f.write('\n'.join(NONE_CHAINS))
+    #Added to handle when no PDB file existed for entry
+    with open('newErrors/NO_PDB_FILE.txt', 'w') as f:
+        f.write('\n'.join(NO_PBD_FILE))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -357,7 +393,7 @@ if __name__ == "__main__":
                     "atom protein structure prediction.")
     parser.add_argument('input_dir', type=str, help='Path to ProteinNet raw records directory.')
     parser.add_argument('-o', '--out_file', type=str, help='Path to output file (.tch file)')
-    parser.add_argument("--pdb_dir", default="/home/jok120/pdb/", type=str,
+    parser.add_argument("--pdb_dir", default="/home/hth6/Downloads/pdb/", type=str,
                         help="Path for ProDy-downloaded PDB files.")
     parser.add_argument('--training_set', type=int, default=100, help='Which thinning of the training set to parse. '
                                                                       '{30,50,70,90,95,100}. Default 100.')
@@ -366,7 +402,7 @@ if __name__ == "__main__":
     VALID_SPLITS = [10, 20, 30, 40, 50, 70, 90]
     TRAIN_FILE = f"training_{args.training_set}.pt"
     PN_TRAIN_DICT, PN_VALID_DICT, PN_TEST_DICT = None, None, None
-    ASTRAL_FILE = "data/dir.des.scope.2.07-stable.txt"
+    ASTRAL_FILE = "data/fullDict.txt" # combined previous versions of dir.des.scope.2.xx-stable.txt into one big dict
     ASTRAL_ID_MAPPING = parse_astral_summary_file(ASTRAL_FILE)
     SUFFIX = str(datetime.datetime.today().strftime("%y%m%d")) + f"_{args.training_set}"
     match = re.search(r"casp\d+", args.input_dir, re.IGNORECASE)
