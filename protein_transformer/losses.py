Improve/correct definition of DRMSD. Did not include normalization by N.
While it may be attractive to use the Frobenius norm, this is not the
right approach, as before taking the sqrt, you must normalize by the
number of attoms.

protein_transformer/losses.py
@@ -230,9 +230,13 @@ def drmsd(a, b, truncate_dist_matrix=True):
 
     if truncate_dist_matrix:
         i = torch.triu_indices(a_.shape[0], a_.shape[1], offset=1)
-        res = torch.norm(a_[i[0], i[1]] - b_[i[0], i[1]])
+        mse = torch.nn.functional.mse_loss(a_[i[0], i[1]].float(), b_[i[0], i[1]].float())
+        res = torch.sqrt(mse)
     else:
-        res = torch.norm(a_ - b_)
+        # TODO remove this option for distance matrix norm
+        mse = torch.nn.functional.mse_loss(a_.float(),
+                                           b_.float())
+        res = torch.sqrt(mse)
 
     return res
 

protein_transformer/tests/test_losses.py
@@ -118,7 +118,8 @@ def test_drmsd_value_symmetric_distances(a, b):
             b_dists.append(np.linalg.norm(c1 - c2))
     a_dists, b_dists = np.asarray(a_dists), np.asarray(b_dists)
 
-    expected_drmsd = np.linalg.norm(a_dists - b_dists)
+    mse = ((a_dists - b_dists) ** 2).mean()
+    expected_drmsd = np.sqrt(mse)
 
     assert drmsd(torch.tensor(a), torch.tensor(b), truncate_dist_matrix=False).item() == approx(expected_drmsd)
 
@@ -172,8 +173,8 @@ def test_drmsd_value_nonsymmetric_distances(a, b):
                 b_dists.append(np.linalg.norm(c1 - c2))
 
     a_dists, b_dists = np.asarray(a_dists), np.asarray(b_dists)
-
-    expected_drmsd = np.linalg.norm(a_dists - b_dists)
+    mse = ((a_dists - b_dists)**2).mean()
+    expected_drmsd = np.sqrt(mse)
 
     assert drmsd(torch.tensor(a), torch.tensor(b),
                  truncate_dist_matrix=True).item() == approx(expected_drmsd)