Fix angle initialization & tanh bug. Modify embeddings w/sqrt scaling. This embedding scaling comes from the Annotated Transformer, though it is unclear why this is done (something with softmax output?). Modify repeat train argument to use the Sampler to "repeat". Add argument to turn off png saving.

protein_transformer/dataset.py
@@ -174,7 +174,7 @@ class SimilarLengthBatchSampler(torch.utils.data.Sampler):
     """
 
     def __init__(self, data_source, batch_size, dynamic_batch, optimize_batch_for_cpus, downsample=None,
-                 use_largest_bin=False):
+                 use_largest_bin=False, repeat_train=None):
         self.data_source = data_source
         self.batch_size = batch_size
         self.dynamic_batch = dynamic_batch
@@ -182,6 +182,7 @@ class SimilarLengthBatchSampler(torch.utils.data.Sampler):
         self.cpu_count = torch.multiprocessing.cpu_count()
         self.downsample = downsample
         self.use_largest_bin = use_largest_bin
+        self.repeat_train =  repeat_train if repeat_train else 1
 
     def __len__(self):
         # If batches are dynamically sized to contain the same number of residues,
@@ -189,11 +190,11 @@ class SimilarLengthBatchSampler(torch.utils.data.Sampler):
         # divided by the size of the dynamic batch.
 
         if self.dynamic_batch:
-            lens_sum = sum(self.data_source.lens)
+            lens_sum = sum(self.data_source.lens) * self.repeat_train
             if self.downsample:
                 lens_sum *=  self.downsample
             return int(np.ceil(lens_sum / self.dynamic_batch))
-        return int(np.ceil(len(self.data_source) / self.batch_size))
+        return int(np.ceil(len(self.data_source) * self.repeat_train / self.batch_size))
 
     def __iter__(self):
         def batch_generator():
@@ -228,9 +229,9 @@ def prepare_dataloaders(data, args, max_seq_len, num_workers=1):
         raise NotImplementedError("Descending and ascending order have not been reimplemented.")
 
     train_dataset = BinnedProteinDataset(
-            seqs=data['train']['seq']*args.repeat_train,
-            crds=data['train']['crd']*args.repeat_train,
-            angs=data['train']['ang']*args.repeat_train,
+            seqs=data['train']['seq'],
+            crds=data['train']['crd'],
+            angs=data['train']['ang'],
             add_sos_eos=args.add_sos_eos, skip_missing_residues=args.skip_missing_res_train, bins=args.bins)
     train_loader = torch.utils.data.DataLoader(
                     train_dataset,
@@ -240,7 +241,8 @@ def prepare_dataloaders(data, args, max_seq_len, num_workers=1):
                                                             args.batch_size,
                                                             dynamic_batch=args.batch_size * MAX_SEQ_LEN,
                                                             optimize_batch_for_cpus=args.loss in ["combined", "drmsd",
-                                                                                                  "ln-drmsd"]))
+                                                                                                  "ln-drmsd"],
+                                                            repeat_train=args.repeat_train))
     train_eval_loader = torch.utils.data.DataLoader(
                     train_dataset,
                     num_workers=num_workers,

protein_transformer/models/encoder_only.py
@@ -25,7 +25,12 @@ class EncoderOnlyTransformer(nn.Module):
         for p in self.parameters():
             if p.dim() > 1:
                 nn.init.xavier_uniform_(p)
-        self.output_projection.bias = nn.Parameter(torch.tensor(np.arctanh(self.angle_means), dtype=torch.float32))
+        if self.use_tanh_out:
+            am = np.arctanh(self.angle_means)
+        else:
+            am = self.angle_means
+        self.output_projection.bias = nn.Parameter(torch.tensor(am, dtype=torch.float32))
+
         nn.init.zeros_(self.output_projection.weight)
 
     def forward(self, enc_input, dec_input=None):

protein_transformer/models/transformer/Decoder.py
@@ -2,12 +2,13 @@ import torch
 
 from .Attention import MultiHeadedAttention
 from .Sublayers import PositionwiseFeedForward, PositionalEncoding, \
-    SublayerConnection
+    SublayerConnection, Embeddings
 
 
 class Decoder(torch.nn.Module):
     """
     Transformer decoder model.
+    # TODO evaluate correctness of using Embeddings layer vs linear (probably correct as is)
     """
 
     def __init__(self, dout, dm, dff, n_heads, n_dec_layers, max_seq_len, dropout=0.1):
@@ -20,7 +21,7 @@ class Decoder(torch.nn.Module):
 
         self.emb_dropout = torch.nn.Dropout(dropout)
         self.positional_enc = PositionalEncoding(dm, dropout, max_seq_len)
-        self.input_embedding = torch.nn.Linear(self.dout, self.dm)
+        self.input_embedding = torch.nn.Linear(self.dout, self.dm)  # Embeddings(self.dout, self.dm)
         self.dec_layers = torch.nn.ModuleList([DecoderLayer(dm, dff, n_heads, dropout) for _ in range(self.n_dec_layers)])
 
     def forward(self, dec_input, enc_output, tgt_mask, src_mask):

protein_transformer/models/transformer/Encoder.py
@@ -2,7 +2,7 @@ import torch
 
 from .Attention import MultiHeadedAttention
 from .Sublayers import PositionwiseFeedForward, PositionalEncoding, \
-    SublayerConnection
+    SublayerConnection, Embeddings
 
 
 class Encoder(torch.nn.Module):
@@ -20,7 +20,7 @@ class Encoder(torch.nn.Module):
         self.max_seq_len = max_seq_len
 
         self.emb_dropout = torch.nn.Dropout(dropout)
-        self.input_embedding = torch.nn.Embedding(self.din, self.dm)
+        self.input_embedding = Embeddings(self.din, self.dm)
         self.positional_enc = PositionalEncoding(dm, dropout, max_seq_len)
 
         self.enc_layers = torch.nn.ModuleList([EncoderLayer(dm, dff, n_heads, dropout) for _ in range(self.n_enc_layers)])

protein_transformer/models/transformer/Transformer.py
@@ -14,7 +14,7 @@ class Transformer(torch.nn.Module):
     """
     def __init__(self, dm, dff, din, dout, n_heads, n_enc_layers, n_dec_layers,
                  max_seq_len, pad_char, missing_coord_filler, device, dropout, fraction_complete_tf,
-                 fraction_subseq_tf, angle_mean_path):
+                 fraction_subseq_tf, angle_means):
         super(Transformer, self).__init__()
         self.din = din
         self.dout = dout
@@ -29,7 +29,7 @@ class Transformer(torch.nn.Module):
         self.device = device
         self.fraction_subseq_tf = fraction_subseq_tf
         self.fraction_complete_tf = fraction_complete_tf
-        self.angle_mean_path = angle_mean_path
+        self.angle_means = angle_means
 
         self.decoder_sos_char = -0.1
 
@@ -109,8 +109,7 @@ class Transformer(torch.nn.Module):
             if p.dim() > 1:
                 torch.nn.init.xavier_uniform_(p)
         # Initialize final projection layer to predict mean of angle distribution
-        angle_means = np.load(self.angle_mean_path)
-        self.output_projection.bias = torch.nn.Parameter(torch.FloatTensor(np.arctanh(angle_means)))
+        self.output_projection.bias = torch.nn.Parameter(torch.FloatTensor(self.angle_means))
         torch.nn.init.xavier_uniform_(self.output_projection.weight, gain=0.00001)
 
 